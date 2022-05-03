0d64fde06cb2cfd3d71967b8b8ad23b76e1c1275
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-03 16:41:40 -0700

    playing with grid

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// chapter 1
+    // 1) set my number to (input) set myName to (input) now swap the values and output 
+    // 2) print -41 - 1001
+    // 3) create a function(input) loop through input number
+    // 4) write for loop for multiples of 3 from -300 - 0 !-3 || -6
+    // 5) Use a while loop to print 0 100
+    // 6) Given 2 numbers if they represent your birthday month and day in any combination log it. 
+    // 7) ...
