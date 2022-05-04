32496f8724fdd70663c18e9a8ba2c74bd4dbe897
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-04 08:53:29 -0700

    object of functions to fecth js code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,43 @@
-$(document).ready(function(){
+var JSFUNCTIONS = {
 
-    console.log("Ready to code");
+    swap: function swap(a,b){
+        a = temp;
+        b = a;
+        a = temp;
+        console.log(a,b);
+    },
+
+    print_range: function print_range(){
+        for( var i = 0; i < 20; i++){
+            console.log(i);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('form').submit(function(){return false;})
+
+    // console.log("Ready to code");
+    $(document).on('click', 'button', function(){
+
+        // $(this).prev().toggle();
+        func = $(this).attr('funcname');
+        console.log(func)
+        // console.log(typeof(func()))
+        // // console.log(func('First', 'Second'))
+        
+        // console.log(JSFUNCTIONS[func]) <----- how to get the js code to console
+        JSFUNCTIONS[func]()
+
+    })
+    // 'input[name="F_Name"'
 
 })
