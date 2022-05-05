344bb6db87454478082f4fa1027882cbca3a2132
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-05 08:19:05 -0700

    one way of grabbing values dynamicly?

--- js/index.js
+++ js/index.js
@@ -13,27 +13,43 @@ var JSFUNCTIONS = {
         }
     },
 
+    compare_arr: function compare_arr(array_1,array_2){
+
+        if (array_1.length != array_2.length) {
+            console.log("lenghts do not match");
+            return false
+        }
+        for (var x = 0; x < array_1.length; x++){
+            console.log(array_1[x]);
+            if (array_1[x] != array_2[x])
+                console.log('Something in them did not match')
+                return false
+        }
+        console.log("They match")
+
+    }
 }
 
 
 
 
-
-
-
-
 $(document).ready(function(){
-    $('form').submit(function(){return false;})
 
     // console.log("Ready to code");
-    $(document).on('click', 'button', function(){
+    // $(document).on('click', 'button', function(){
+        $('form').submit(function(){
+            console.log($(this))
+            console.log($(this)[0][1].value)
+            console.log($(this)[0][0].value)
+            console.log($(this)[0][2].attributes[0].value)
 
-        func = $(this).attr('funcname');
-        
-        $(this).prev().html('<span class="code">' + JSFUNCTIONS[func] + '</span>')
-        $(this).prev().toggle();
+        // func = $(this).attr('funcname');
+        // // console.log($(this).siblings())
+        // $()
+        // $(this).prev().html('<span class="code">' + JSFUNCTIONS[func] + '</span>')
+        // $(this).prev().toggle();
 
-
+        return false
     })
     // 'input[name="F_Name"'
 
